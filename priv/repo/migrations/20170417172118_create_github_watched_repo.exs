b9412bc6946cf2c1f1e1c5b52191704e70b9fc03
Bachir Çaoui <bachir@followanalytics.com>
2017-04-19 10:28:22 +0200

    Remove timestamp from WatchedRepo and add unique index

--- priv/repo/migrations/20170417172118_create_github_watched_repo.exs
+++ priv/repo/migrations/20170417172118_create_github_watched_repo.exs
@@ -6,10 +6,9 @@ defmodule Dev3.Repo.Migrations.CreateDev3.GitHub.WatchedRepo do
       add :github_id, :integer, null: false
       add :full_name, :string, null: false
       add :user_id, references(:users, on_delete: :delete_all, type: :binary_id), null: false
-
-      timestamps()
     end
 
     create index(:watched_repos, [:user_id])
+    create unique_index(:watched_repos, [:user_id, :full_name])
   end
 end
