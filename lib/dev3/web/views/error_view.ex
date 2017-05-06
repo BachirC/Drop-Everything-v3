d9654e0cd847457d10fa854f379ae1fe111d3712
Bachir Çaoui <bachir@followanalytics.com>
2017-05-06 17:19:20 +0200

    Update error views

--- lib/dev3/web/views/error_view.ex
+++ lib/dev3/web/views/error_view.ex
@@ -20,14 +20,6 @@ defmodule Dev3.Web.ErrorView do
     %{errors: %{detail: "Internal server error"}}
   end
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
-
-  def render("500.html", _assigns) do
-    "Internal server error"
-  end
-
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
--- test/web/views/error_view_test.exs
+++ test/web/views/error_view_test.exs
@@ -4,18 +4,23 @@ defmodule Dev3.Web.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(Dev3.Web.ErrorView, "404.html", []) ==
-           "Page not found"
+  test "renders 401.json" do
+    assert render(Dev3.Web.ErrorView, "401.json", []) ==
+      %{errors: %{detail: "Unauthorized"}}
   end
 
-  test "render 500.html" do
-    assert render_to_string(Dev3.Web.ErrorView, "500.html", []) ==
-           "Internal server error"
+  test "renders 404.json" do
+    assert render(Dev3.Web.ErrorView, "404.json", []) ==
+      %{errors: %{detail: "Page not found"}}
+  end
+
+  test "render 500.json" do
+    assert render(Dev3.Web.ErrorView, "500.json", []) ==
+      %{errors: %{detail: "Internal server error"}}
   end
 
   test "render any other" do
-    assert render_to_string(Dev3.Web.ErrorView, "505.html", []) ==
-           "Internal server error"
+    assert render(Dev3.Web.ErrorView, "500.json", []) ==
+      %{errors: %{detail: "Internal server error"}}
   end
 end
