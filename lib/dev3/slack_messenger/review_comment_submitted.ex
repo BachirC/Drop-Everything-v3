3e3b1ff9569e6b365cd994767e08a54187875107
Bachir Çaoui <bachir@followanalytics.com>
2017-05-09 14:38:50 +0200

    Add GitHub PR messages

---
+++ lib/dev3/slack_messenger/review_comment_submitted.ex
@@ -0,0 +1,21 @@
+defmodule Dev3.SlackMessenger.HTTPClient.ReviewCommentSubmitted do
+  @behaviour Dev3.SlackMessenger.HTTPClient
+
+  def build_message(data) do
+   %{text: "A Comment has been submitted to your Pull Request", attachments: build_attachments(data)}
+  end
+
+  defp build_attachments(data) do
+    [%{
+      "title" => "PR link",
+      "title_link" => "https://google.fr",
+      "text" => "",
+      "author_name" => "Me",
+      "author_icon" => "",
+      "footer" => "footer",
+      "footer_icon" => "",
+      "color" => "#ef0e02",
+      "ts" => 1494261981
+    }]
+  end
+end
