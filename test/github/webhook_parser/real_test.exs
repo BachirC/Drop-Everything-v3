24aca227e0322186ec5dd349a1b3090c93b42958
Bachir Çaoui <bachir@followanalytics.com>
2017-05-17 10:21:15 +0200

    Add more tests to WebhookParser

--- test/github/webhook_parser/real_test.exs
+++ test/github/webhook_parser/real_test.exs
@@ -4,14 +4,19 @@ defmodule Dev3.GitHub.WebhookParser.RealTest do
   alias Dev3.Repo
   alias Dev3.User
   alias Dev3.GitHub.WatchedRepo
+  alias Dev3.GitHub.MutedIssue
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
   end
 
+  test "returns an error when message_type not found" do
+    assert Real.parse(:action_non_handled, %{}) == {:unhandled_message_type, :action_non_handled}
+  end
+
   test "review_requested" do
     user = Repo.insert!(%User{github_id: 1, slack_team_id: "ST1", slack_user_id: "SU1", slack_access_token: "SAT1"})
-    repo = insert_watched_repo(user)
+    insert_watched_repo(user)
     payload = Path.expand("../../fixtures/github_api/webhooks/pull_request/review_requested.json", __DIR__)
               |> File.read!()
               |> Poison.decode!()
@@ -25,7 +30,7 @@ defmodule Dev3.GitHub.WebhookParser.RealTest do
                 type: :pull_request,
                 url: "https://github.com/baxterthehacker/public-repo/pull/1"},
         owner: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3"},
-        repo: %{id: repo.github_id,
+        repo: %{id: 35129377,
                 name: "baxterthehacker/public-repo",
                 url: "https://github.com/baxterthehacker/public-repo"},
         sender: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3",
@@ -38,7 +43,7 @@ defmodule Dev3.GitHub.WebhookParser.RealTest do
 
   test "review_submitted" do
     user = Repo.insert!(%User{github_id: 2546, slack_team_id: "ST1", slack_user_id: "SU1", slack_access_token: "SAT1"})
-    repo = insert_watched_repo(user)
+    insert_watched_repo(user)
     payload = Path.expand("../../fixtures/github_api/webhooks/pull_request_review/submitted.json", __DIR__)
               |> File.read!()
               |> Poison.decode!()
@@ -52,7 +57,7 @@ defmodule Dev3.GitHub.WebhookParser.RealTest do
                           type: :pull_request,
                           url: "https://github.com/baxterthehacker/public-repo/pull/8"},
                   owner: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3"},
-                  repo: %{id: repo.github_id,
+                  repo: %{id: 35129377,
                           name: "baxterthehacker/public-repo",
                           url: "https://github.com/baxterthehacker/public-repo"},
                   sender: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3",
@@ -68,7 +73,7 @@ defmodule Dev3.GitHub.WebhookParser.RealTest do
 
   test "tagged_in_issue" do
     user = Repo.insert!(%User{github_id: 1, github_user_id: "BachirC", slack_team_id: "ST1", slack_user_id: "SU1", slack_access_token: "SAT1"})
-    repo = insert_watched_repo(user)
+    insert_watched_repo(user)
     payload = Path.expand("../../fixtures/github_api/webhooks/issues/opened.json", __DIR__)
               |> File.read!()
               |> Poison.decode!()
@@ -81,7 +86,7 @@ defmodule Dev3.GitHub.WebhookParser.RealTest do
                           type: :issue,
                           url: "https://github.com/baxterthehacker/public-repo/issues/2"},
                   owner: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3"},
-                  repo: %{id: repo.github_id,
+                  repo: %{id: 35129377,
                           name: "baxterthehacker/public-repo",
                           url: "https://github.com/baxterthehacker/public-repo"},
                   sender: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3",
@@ -94,7 +99,7 @@ defmodule Dev3.GitHub.WebhookParser.RealTest do
 
   test "tagged_in_issue_comment" do
     user = Repo.insert!(%User{github_id: 1, github_user_id: "BachirC", slack_team_id: "ST1", slack_user_id: "SU1", slack_access_token: "SAT1"})
-    repo = insert_watched_repo(user)
+    insert_watched_repo(user)
     payload = Path.expand("../../fixtures/github_api/webhooks/issue_comment/created.json", __DIR__)
               |> File.read!()
               |> Poison.decode!()
@@ -107,7 +112,7 @@ defmodule Dev3.GitHub.WebhookParser.RealTest do
                           type: :issue,
                           url: "https://github.com/baxterthehacker/public-repo/issues/2"},
                   owner: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3"},
-                  repo: %{id: repo.github_id,
+                  repo: %{id: 35129377,
                           name: "baxterthehacker/public-repo",
                           url: "https://github.com/baxterthehacker/public-repo"},
                   sender: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3",
@@ -120,8 +125,60 @@ defmodule Dev3.GitHub.WebhookParser.RealTest do
     assert res == expected
   end
 
-  test "returns an error when message_type not found" do
-    assert Real.parse(:action_non_handled, %{}) == {:unhandled_message_type, :action_non_handled}
+  test "Doesn't target the user if he is not watching the repo" do
+    Repo.insert!(%User{github_id: 1, github_user_id: "BachirC", slack_team_id: "ST1", slack_user_id: "SU1", slack_access_token: "SAT1"})
+    payload = Path.expand("../../fixtures/github_api/webhooks/issue_comment/created.json", __DIR__)
+              |> File.read!()
+              |> Poison.decode!()
+    expected = {:ok,
+                [],
+                %{issue: %{body: "It looks like you accidently spelled 'commit' with two 't's.",
+                          id: 73464126,
+                          number: 2,
+                          title: "Spelling error in the README file",
+                          type: :issue,
+                          url: "https://github.com/baxterthehacker/public-repo/issues/2"},
+                  owner: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3"},
+                  repo: %{id: 35129377,
+                          name: "baxterthehacker/public-repo",
+                          url: "https://github.com/baxterthehacker/public-repo"},
+                  sender: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3",
+                            name: "baxterthehacker"},
+                  comment: %{url: "https://github.com/baxterthehacker/public-repo/issues/2#issuecomment-99262140",
+                             body: "You have been tagged @BachirC"}}}
+
+    res = Real.parse(:tagged_in_issue_comment, payload)
+
+    assert res == expected
+  end
+
+  test "Doesn't target the user if he muted the issue" do
+    user = Repo.insert!(%User{github_id: 1, github_user_id: "BachirC", slack_team_id: "ST1", slack_user_id: "SU1", slack_access_token: "SAT1"})
+    repo = insert_watched_repo(user)
+    Repo.insert!(%MutedIssue{github_id: 73464126, title: "Spelling error in the README file", repo_id: repo.id, user_id: user.id})
+    payload = Path.expand("../../fixtures/github_api/webhooks/issue_comment/created.json", __DIR__)
+              |> File.read!()
+              |> Poison.decode!()
+    expected = {:ok,
+                [],
+                %{issue: %{body: "It looks like you accidently spelled 'commit' with two 't's.",
+                          id: 73464126,
+                          number: 2,
+                          title: "Spelling error in the README file",
+                          type: :issue,
+                          url: "https://github.com/baxterthehacker/public-repo/issues/2"},
+                  owner: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3"},
+                  repo: %{id: 35129377,
+                          name: "baxterthehacker/public-repo",
+                          url: "https://github.com/baxterthehacker/public-repo"},
+                  sender: %{avatar_url: "https://avatars.githubusercontent.com/u/6752317?v=3",
+                            name: "baxterthehacker"},
+                  comment: %{url: "https://github.com/baxterthehacker/public-repo/issues/2#issuecomment-99262140",
+                             body: "You have been tagged @BachirC"}}}
+
+    res = Real.parse(:tagged_in_issue_comment, payload)
+
+    assert res == expected
   end
 
   defp insert_watched_repo(user) do
