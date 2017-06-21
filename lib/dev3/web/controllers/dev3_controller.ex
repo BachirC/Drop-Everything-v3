787792b4b1dd2522d6fadf7a44333e685f7f7512
Bachir Çaoui <bachir.caoui@gmail.com>
2017-06-21 21:09:32 +0200

    Default redirect to homepage

--- lib/dev3/web/controllers/dev3_controller.ex
+++ lib/dev3/web/controllers/dev3_controller.ex
@@ -2,6 +2,6 @@ defmodule Dev3.Web.Dev3Controller do
   use Dev3.Web, :controller
 
   def index(conn, _) do
-    redirect conn, external: "https://dev3.bachirc.me"
+    redirect conn, to: "/gitbruh.html"
   end
 end
