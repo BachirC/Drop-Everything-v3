ec12aa74ed2d111f1c01f72874065f3f6b307152
Bachir Çaoui <bachir.caoui@gmail.com>
2017-06-23 18:18:14 +0200

    More fixes on homepage redirection

--- lib/dev3/web/controllers/auth_fallback_controller.ex
+++ lib/dev3/web/controllers/auth_fallback_controller.ex
@@ -4,21 +4,21 @@ defmodule Dev3.Web.AuthFallbackController do
 
   def call(conn, %{token: %{other_params: %{"error" => error}}}) do
     Logger.error fn -> "GitHub OAuth error : #{error}" end
-    redirect(conn, to: "/dev3.html")
+    redirect(conn, to: "/gitbruh.html")
   end
 
   def call(conn, %{token: %{other_params: %{"ok" => false, "error" => error}}}) do
     Logger.error fn -> "Slack OAuth error : #{error}" end
-    redirect(conn, to: "/dev3.html")
+    redirect(conn, to: "/gitbruh.html")
   end
 
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
     Logger.error fn -> "Error on save: #{inspect changeset}" end
-    redirect(conn, to: "/dev3.html")
+    redirect(conn, to: "/gitbruh.html")
   end
 
   def call(conn, err) do
     Logger.error fn -> "Error : #{inspect err}" end
-    redirect(conn, to: "/dev3.html")
+    redirect(conn, to: "/gitbruh.html")
   end
 end
